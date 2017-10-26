014966d0b73d2f9e064c62a24b5ceabc005dc73e
yihuan2016 <yihuan@seas.upenn.edu>
2017-10-26 15:29:18 -0400

    Source

---
+++ Source/paintball/paintballHUD.h
@@ -0,0 +1,25 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once 
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "paintballHUD.generated.h"
+
+UCLASS()
+class ApaintballHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	ApaintballHUD();
+
+	/** Primary draw call for the HUD */
+	virtual void DrawHUD() override;
+
+private:
+	/** Crosshair asset pointer */
+	class UTexture2D* CrosshairTex;
+
+};
+
